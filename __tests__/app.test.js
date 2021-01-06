32ea7193e5bb3ef72322839b11fca51141c86e9b
kevinfiero <kjf46@cornell.edu>
2021-01-06 13:57:16 -0800

    first test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,26 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe(' routes', () => {
+describe('demo routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('allows a user to signup via POST', async() => {
+   
+    const res = await request(app)
+      .post('/api/v1/auth/signup') //setup post route
+      .send({ email: 'test@test.com', password: 'password' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com',
+      password: 'password'
+    });
+
   });
 });
