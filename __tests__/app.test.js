0b9a41c592ad5171eb1f70e537a96575a91a1af9
ErikGraciosa <erikgraciosa@gmail.com>
2021-01-07 14:15:55 -0800

    added Gram tests and routes and model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -74,4 +74,104 @@ describe('demo routes', () => {
       profilePhotoUrl: 'https://www.placecage.com/200/300'
     });
   });
+
+  it('/POST for new gram', async() => {
+    const agent = request.agent(app);
+    const user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const res = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    expect(res.body).toEqual({
+      userId: user.userId,
+      photoUrl: 'https://www.fillmurray.com/200/300',
+      caption: 'Cool!',
+      tags: ['funny', 'snl'],
+      gramId: expect.any(String)
+    });
+  });
+
+  it('/GET a list of grams', async() => {
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+    
+    const user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    const user2 = await UserService.create({
+      email: 'test2@test.com',
+      password: 'password2',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    await agent2
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test2@test.com',
+        password: 'password2'
+      });
+
+    await agent2
+    .post('/api/v1/grams')
+    .send({
+      photoUrl: 'https://www.placekitten.com/200/300',
+      caption: 'So fluff',
+      tags: ['cat', 'smol']
+    });
+
+    const res = await request(app)
+      .get('/api/v1/grams');
+
+    const results = [{
+        userId: user.userId,
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl'],
+        gramId: "1"
+      },
+      {
+        userId: user2.userId,
+        photoUrl: 'https://www.placekitten.com/200/300',
+        caption: 'So fluff',
+        tags: ['cat', 'smol'],
+        gramId: "2"
+      }
+    ];
+
+    results.forEach(result => expect(res.body).toContainEqual(result));
+  });
 });
