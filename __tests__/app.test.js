61a77021f5845c314a443ed1ffb345f1e4b257cd
kevinfiero <kjf46@cornell.edu>
2021-01-07 15:34:07 -0800

    completed comments model and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -146,32 +146,213 @@ describe('demo routes', () => {
       });
 
     await agent2
-    .post('/api/v1/grams')
-    .send({
-      photoUrl: 'https://www.placekitten.com/200/300',
-      caption: 'So fluff',
-      tags: ['cat', 'smol']
-    });
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.placekitten.com/200/300',
+        caption: 'So fluff',
+        tags: ['cat', 'smol']
+      });
 
     const res = await request(app)
       .get('/api/v1/grams');
 
     const results = [{
-        userId: user.userId,
-        photoUrl: 'https://www.fillmurray.com/200/300',
-        caption: 'Cool!',
-        tags: ['funny', 'snl'],
-        gramId: "1"
-      },
-      {
-        userId: user2.userId,
-        photoUrl: 'https://www.placekitten.com/200/300',
-        caption: 'So fluff',
-        tags: ['cat', 'smol'],
-        gramId: "2"
-      }
+      userId: user.userId,
+      photoUrl: 'https://www.fillmurray.com/200/300',
+      caption: 'Cool!',
+      tags: ['funny', 'snl'],
+      gramId: expect.any(String)
+    },
+    {
+      userId: user2.userId,
+      photoUrl: 'https://www.placekitten.com/200/300',
+      caption: 'So fluff',
+      tags: ['cat', 'smol'],
+      gramId: expect.any(String)
+    }
     ];
 
     results.forEach(result => expect(res.body).toContainEqual(result));
   });
+
+  it('/POST for new comment', async() => {
+    const agent = request.agent(app);
+    await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    const res = await agent
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    expect(res.body).toEqual({
+      commentId: expect.any(String),
+      comment: 'Wow looks fun!',
+      userId: expect.any(String),
+      gramId: gramPosted.body.gramId
+    });
+  });
+
+  it('/DELETE comment by id', async() => {
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+
+    const user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    const user2 = await UserService.create({
+      email: 'test2@test.com',
+      password: 'password2',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    await agent2
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test2@test.com',
+        password: 'password2'
+      });
+
+    const commentPosted = await agent2
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    const res = await agent2
+      .delete(`/api/v1/comments/${commentPosted.body.commentId}`);
+
+    expect(res.body).toEqual(commentPosted.body);
+
+  });
+
+  it('/DELETE user 1 tries to delete user 2 comment', async() => {
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+
+    const user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    const user2 = await UserService.create({
+      email: 'test2@test.com',
+      password: 'password2',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    await agent2
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test2@test.com',
+        password: 'password2'
+      });
+
+    const commentPosted = await agent2
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    const res = await agent
+      .delete(`/api/v1/comments/${commentPosted.body.commentId}`);
+
+    expect(res.body).toEqual({
+      'message': 'No comment with id 1 is valid user 1 to delete',
+      'status': 500
+    });
+
+  });
+
+  // it('/GET a list of grams', async() => {
+  //   const agent = request.agent(app);
+    
+  //   const user = await UserService.create({
+  //     email: 'test@test.com',
+  //     password: 'password',
+  //     profilePhotoUrl: 'https://www.placecage.com/200/300'
+  //   });
+
+  //   await agent
+  //     .post('/api/v1/auth/login')
+  //     .send({
+  //       email: 'test@test.com',
+  //       password: 'password'
+  //     });
+
+  //   await agent
+  //     .post('/api/v1/grams')
+  //     .send({
+  //       photoUrl: 'https://www.fillmurray.com/200/300',
+  //       caption: 'Cool!',
+  //       tags: ['funny', 'snl']
+  //     });
+
+  //   const res = await request(app)
+  //     .get('/api/v1/grams/:id');
+
+  //   expect(res.body).toEqual({
+  //     userId: user.userId,
+  //     photoUrl: 'https://www.fillmurray.com/200/300',
+  //     caption: 'Cool!',
+  //     tags: ['funny', 'snl'],
+  //     gramId: expect.any(String)
+  //   });
+  // });
 });
