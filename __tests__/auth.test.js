bdfa6fd2d5e959bf3938c3c5c1d1fcd852391de6
Franco Ortega <francopdx@gmail.com>
2021-01-11 16:15:33 -0800

    add grams.test.js and bonus.test.js

---
+++ __tests__/auth.test.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
+
+let user;
+
+describe('auth routes', () => {
+  beforeEach(async() => {
+    pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+
+    return user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('/POST signup', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup') //setup post route
+      .send({
+        email: 'test2@test.com',
+        password: 'password',
+        profilePhotoUrl: 'https://www.placecage.com/200/300'
+      });
+
+    expect(res.body).toEqual({
+      userId: expect.any(String),
+      email: 'test2@test.com',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+  });
+
+  it('/POST login', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      userId: user.userId,
+      email: 'test@test.com',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+  });
+
+  it('/GET verify', async() => {
+    const agent = request.agent(app);
+    
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const res = await agent 
+      .get('/api/v1/auth/verify');
+
+    expect(res.body).toEqual({
+      userId: user.userId,
+      email: 'test@test.com',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+  });
+});
---
+++ __tests__/comments.test.js
@@ -0,0 +1,182 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  it('/POST for new comment', async() => {
+    const agent = request.agent(app);
+    await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    const res = await agent
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    expect(res.body).toEqual({
+      commentId: expect.any(String),
+      comment: 'Wow looks fun!',
+      userId: expect.any(String),
+      gramId: gramPosted.body.gramId
+    });
+  });
+
+  it('/DELETE comment by id', async() => {
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+
+    await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await UserService.create({
+      email: 'test2@test.com',
+      password: 'password2',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    await agent2
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test2@test.com',
+        password: 'password2'
+      });
+
+    const commentPosted = await agent2
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    const res = await agent2
+      .delete(`/api/v1/comments/${commentPosted.body.commentId}`);
+
+    expect(res.body).toEqual(commentPosted.body);
+
+  });
+
+  it('/DELETE user 1 tries to delete user 2 comment', async() => {
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+
+    await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await UserService.create({
+      email: 'test2@test.com',
+      password: 'password2',
+      profilePhotoUrl: 'https://www.placecage.com/200/300'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const gramPosted = await agent
+      .post('/api/v1/grams')
+      .send({
+        photoUrl: 'https://www.fillmurray.com/200/300',
+        caption: 'Cool!',
+        tags: ['funny', 'snl']
+      });
+
+    await agent2
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test2@test.com',
+        password: 'password2'
+      });
+
+    const commentPosted = await agent2
+      .post('/api/v1/comments')
+      .send({
+        comment: 'Wow looks fun!',
+        gramId: gramPosted.body.gramId
+      });
+
+    const res = await agent
+      .delete(`/api/v1/comments/${commentPosted.body.commentId}`);
+
+    expect(res.body).toEqual({
+      'message': 'No comment with id 1 is valid for user 1 to delete',
+      'status': 500
+    });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
